c9cf29997f67ab3a1b7f34925d7f7e94b5296909
Priscila Dalepiane <prisciladalepiane@gmail.com>
2023-11-06 07:48:15 -0400

    Agrupado ui e server em um arquivo

--- app/app.R
+++ app/app.R
@@ -1,7 +1,76 @@
+###########################  Carregar Pacotes ##################################
+
 library(shiny)
-library(shinythemes)
+library(ltm)
+
+###########################  Definir Variáveis ################################# 
 
 
 questoes <- 1:20
+areas <- c("Linguagem", "Matemática")
 
-runApp('app')
+
+###########################  Interface Usuario ################################# 
+
+navbarPage("Unicesumar - TCC",
+           tabPanel("Component 1",
+                    sidebarLayout(
+                      
+                      sidebarPanel(
+                        
+                        wellPanel(
+                          selectInput("selArea", "Selecione a área:", areas)
+                        )
+                        
+                      ),
+                      
+                      mainPanel(
+                        tabsetPanel(
+                          tabPanel("1", plotOutput("distPlot")),
+                          tabPanel("2", plotOutput("distPlot")),
+                          tabPanel("3", plotOutput("distPlot"))
+                        )
+                      )
+                      
+                    )
+                    
+                    
+           ),
+           tabPanel("Índices TCT",
+                    plotOutput("distPlot")
+                    
+           ),
+           tabPanel("Análise por questão",
+                    plotOutput("distPlot")
+                    
+           ),
+           navbarMenu("More",
+                      tabPanel("Sub-Component A"),
+                      tabPanel("Sub-Component B"))
+)
+
+###########################  Server  ###########################################
+
+
+server <- function(input, output, session) {
+  
+  output$distPlot <- renderPlot({
+    
+    # generate bins based on input$bins from ui.R
+    x    <- faithful[, 2]
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    
+    # draw the histogram with the specified number of bins
+    hist(x, breaks = bins, col = 'darkgray', border = 'white',
+         xlab = 'Waiting time to next eruption (in mins)',
+         main = 'Histogram of waiting times')
+    
+  })
+  
+}
+
+
+
+###########################  Rodar Aplicacao ################################### 
+
+shinyApp(ui = ui, server = server)
