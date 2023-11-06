897e83159109c2159301b2dc4b97bb4ecc5b83b0
Priscila Dalepiane <prisciladalepiane@gmail.com>
2023-11-06 10:13:38 -0400

    Adicionado ajuste de css

--- app/app.R
+++ app/app.R
@@ -2,6 +2,10 @@
 
 library(shiny)
 library(ltm)
+library(fresh)
+library(shinyWidgets)
+library(htmltools)
+
 
 ###########################  Definir Variáveis ################################# 
 
@@ -15,7 +19,23 @@ lsat.desc<-descript(LSAT)
 
 ###########################  Interface Usuario ################################# 
 
-ui <- navbarPage("Unicesumar - TCC",
+
+ui <- navbarPage("Unicesumar",
+             header = use_theme(
+               create_theme(
+                 theme = "default",
+                 bs_vars_navbar(
+                   padding_horizontal = "15px",
+                   default_bg = "#1122BB",
+                   default_color = "#FFFFFF",
+                   default_link_color = "#FFFFFF",
+                   default_link_active_color = "#91FFFF",
+                   default_link_hover_color = "#A4A4A4"
+                 ),
+                 output_file = NULL
+               )
+             ),
+
            tabPanel("Início",
                     sidebarLayout(
                       
@@ -45,6 +65,7 @@ ui <- navbarPage("Unicesumar - TCC",
                     plotOutput("distPlot")
                     
            )
+           
 )
 
 ###########################  Server  ###########################################
@@ -54,14 +75,7 @@ server <- function(input, output, session) {
   
   output$distPlot <- renderPlot({
     
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2]
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white',
-         xlab = 'Waiting time to next eruption (in mins)',
-         main = 'Histogram of waiting times')
+    plot(iris)
     
   })
   
