c21ace1ef3c1b7098971df23de900a0579b6121e
Priscila Dalepiane <prisciladalepiane@gmail.com>
2023-11-06 09:37:05 -0400

    Ajustando Saídas

--- app/app.R
+++ app/app.R
@@ -9,11 +9,14 @@ library(ltm)
 questoes <- 1:20
 areas <- c("Linguagem", "Matemática")
 
+data(LSAT)
+lsat.desc<-descript(LSAT)
+
 
 ###########################  Interface Usuario ################################# 
 
-navbarPage("Unicesumar - TCC",
-           tabPanel("Component 1",
+ui <- navbarPage("Unicesumar - TCC",
+           tabPanel("Início",
                     sidebarLayout(
                       
                       sidebarPanel(
@@ -26,27 +29,22 @@ navbarPage("Unicesumar - TCC",
                       
                       mainPanel(
                         tabsetPanel(
-                          tabPanel("1", plotOutput("distPlot")),
-                          tabPanel("2", plotOutput("distPlot")),
-                          tabPanel("3", plotOutput("distPlot"))
-                        )
+                          plotOutput("distPlot")
+
                       )
                       
                     )
                     
-                    
+                    )       
            ),
            tabPanel("Índices TCT",
-                    plotOutput("distPlot")
+                    tableOutput("tbDescript")
                     
            ),
            tabPanel("Análise por questão",
                     plotOutput("distPlot")
                     
-           ),
-           navbarMenu("More",
-                      tabPanel("Sub-Component A"),
-                      tabPanel("Sub-Component B"))
+           )
 )
 
 ###########################  Server  ###########################################
@@ -66,6 +64,9 @@ server <- function(input, output, session) {
          main = 'Histogram of waiting times')
     
   })
+  
+  output$tbMatriz <- renderTable(LSAT)
+  output$tbDescript <- renderTable(lsat.desc$bisCorr)
   
 }
 
