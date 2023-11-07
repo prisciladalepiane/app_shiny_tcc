18bf4ca9a2460eead4218dcbf57157ece6b563f5
Priscila Dalepiane <prisciladalepiane@gmail.com>
2023-11-07 15:27:06 -0400

    Corrigido erro que excluia um item do cálculo

--- model/funcoes.R
+++ model/funcoes.R
@@ -2,18 +2,18 @@
 calcularTCT <- function(matriz){
   
   
-  difi <- colMeans(matriz[,-1], na.rm = TRUE) %>% as.data.frame()
+  difi <- colMeans(matriz, na.rm = TRUE) %>% as.data.frame()
   names(difi) <- "DIFI"
   difi$Item <- row.names(difi) 
   rownames(difi) <- NULL
   
-  p_acerto <- rowMeans(matriz[,-1]) 
+  p_acerto <- rowMeans(matriz) 
   
   respostas1 <- matriz %>% mutate(p_acerto) %>% 
     filter(p_acerto <= quantile(p_acerto,0.27)) %>% 
     dplyr::select(-p_acerto)
   
-  abai <- colMeans(respostas1[,-1], na.rm = TRUE) %>%
+  abai <- colMeans(respostas1, na.rm = TRUE) %>%
     as.data.frame() 
   names(abai) <- "ABAI"
   abai$Item <- row.names(abai) 
@@ -23,13 +23,13 @@ calcularTCT <- function(matriz){
     filter(p_acerto >= quantile(p_acerto,0.73)) %>% 
     dplyr::select(-p_acerto)
   
-  acim <- colMeans(respostas2[,-1], na.rm = TRUE) %>%
+  acim <- colMeans(respostas2, na.rm = TRUE) %>%
     as.data.frame() 
   names(acim) <- "ACIM"
   acim$Item <- row.names(acim) 
   rownames(acim) <- NULL
   
-  tct <- ltm::descript(matriz[,-1])
+  tct <- ltm::descript(matriz)
   
   bisCorr <- data.frame(Item = names(tct$bisCor),  BIS = tct$bisCorr)
   
