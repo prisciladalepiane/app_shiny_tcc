c67be9d1dc309b4dccc5cbaddfea9362c4f2f66a
Priscila Dalepiane <prisciladalepiane@gmail.com>
2023-11-07 09:31:07 -0400

    Alternado nome de variácel de CodigoItem para Item

--- model/funcoes.R
+++ model/funcoes.R
@@ -4,7 +4,7 @@ calcularTCT <- function(matriz){
   
   difi <- colMeans(matriz[,-1], na.rm = TRUE) %>% as.data.frame()
   names(difi) <- "DIFI"
-  difi$CodigoItem <- row.names(difi) 
+  difi$Item <- row.names(difi) 
   rownames(difi) <- NULL
   
   p_acerto <- rowMeans(matriz[,-1]) 
@@ -16,7 +16,7 @@ calcularTCT <- function(matriz){
   abai <- colMeans(respostas1[,-1], na.rm = TRUE) %>%
     as.data.frame() 
   names(abai) <- "ABAI"
-  abai$CodigoItem <- row.names(abai) 
+  abai$Item <- row.names(abai) 
   rownames(abai) <- NULL
   
   respostas2 <- matriz %>% mutate(p_acerto) %>% 
@@ -26,16 +26,16 @@ calcularTCT <- function(matriz){
   acim <- colMeans(respostas2[,-1], na.rm = TRUE) %>%
     as.data.frame() 
   names(acim) <- "ACIM"
-  acim$CodigoItem <- row.names(acim) 
+  acim$Item <- row.names(acim) 
   rownames(acim) <- NULL
   
   tct <- ltm::descript(matriz[,-1])
   
-  bisCorr <- data.frame(CodigoItem = names(tct$bisCor),  BIS = tct$bisCorr)
+  bisCorr <- data.frame(Item = names(tct$bisCor),  BIS = tct$bisCorr)
   
   tct_questoes <- left_join(difi, abai) %>% 
     left_join(acim) %>%
-    dplyr::select(CodigoItem, DIFI, ABAI, ACIM) %>%
+    dplyr::select(Item, DIFI, ABAI, ACIM) %>%
     mutate(DISCR = ACIM - ABAI) %>%
     left_join(bisCorr) %>%
     suppressMessages()
