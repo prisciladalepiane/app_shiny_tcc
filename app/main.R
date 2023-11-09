216bb49519cae7f1685c757e24c8fdd8899735fe
Priscila Dalepiane <prisciladalepiane@gmail.com>
2023-11-09 13:18:57 -0400

    Teste para mostrar análise por alternativas

--- app/main.R
+++ app/main.R
@@ -17,11 +17,25 @@ source("server.R")
 questoes <- 1:20
 areas <- c("Linguagem", "Matemática")
 
-data(LSAT)
 
-matriz <- LSAT
+teste <- read.csv2("C:/tcc2_eng/respostas_teste.csv")
+gabarito <- read.csv2("C:/tcc2_eng/gabarito_teste.csv")
 
+questoes <- gabarito$Item
 
+matriz <- respostasParaMatriz(teste) %>% select(-RespondenteId)
+
+matriz_alternativas <- respostasParaMatriz(teste, alternativas = T)
+
+tct_alt <- calculoTctAlternativas(matriz_alternativas, gabarito)
+
+tct_alternativas <- tct_alt  |>
+  select(Item, correct, key, n, rspP, pBis) %>%  
+  mutate_if(is.double, ~round(.,2)) |>
+  mutate(rspP = paste0(rspP * 100, "%")) |> distinct() |> 
+  gather(key = "Alternativa", value = "Valores", -c(Item, key)) |> 
+  spread(key = key, value = Valores) |>
+  arrange(Item, factor(Alternativa, levels = c("correct", "n", "rspP", "pBis")))
 
 ###########################  Rodar Aplicacao ################################### 
 
