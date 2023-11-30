d73b390c6d19b10d98112e72aa4d9e70125b5158
Priscila Dalepiane <prisciladalepiane@gmail.com>
2023-11-30 19:10:08 -0400

    Adicionado evento reativo para tabelas de resposta e gabarito

--- app/server.R
+++ app/server.R
@@ -1,6 +1,53 @@
 server <- function(input, output, session) {
   
+  
   ###  Cálculos 
+  
+  resposta <- teste |> mutate(CodigoQuestao = str_sub(CodigoQuestao,1,3))
+  gabarito <- gabarito_teste |> mutate(Item = str_sub(Item,1,3))
+  
+  respostaReac <- reactive({
+    if(is.null(input$file1)){
+      resposta
+    }else{
+      read.csv2(input$file1$datapath)
+    }
+  })
+  
+  gabaritoReac <- reactive({
+    if(is.null(input$file2)){
+     gabarito
+    }else{
+     read.csv2(input$file2$datapath)
+    }
+  })
+  
+
+  questoes <- gabarito$Item
+  
+  matriz <- respostasParaMatriz(respostas) 
+  
+  matriz_alternativas <- respostasParaMatriz(respostas, alternativas = T)
+  
+  tct_alt <- calculoTctAlternativas(matriz_alternativas, gabarito) 
+  
+  tct_alt <- tct_alt %>% 
+    filter(key > 0) %>% 
+    mutate(key = LETTERS[as.numeric(key)]) 
+  
+  tct_alternativas <- formatarTctAlternativas(tct_alt)
+  
+  acertos <- matriz |> select(RespondenteId) |> mutate(Acertos = rowSums(matriz[,-1]))
+  
+  grafico_alt <- respostas |> left_join(acertos) |>
+    group_by(Acertos,CodigoQuestao,AlternativaOrdem) |> 
+    count() |>
+    mutate(Alternativa = LETTERS[AlternativaOrdem]) 
+  
+  grafico_alt2 <- grafico_alt |> left_join(
+    grafico_alt |> group_by(CodigoQuestao,Acertos) |> summarise(Total = sum(n))
+  ) |> mutate(p = n/Total) 
+  
   
   tct <- calcularTCT(matriz[,-1])
   
@@ -43,29 +90,13 @@ server <- function(input, output, session) {
                      pageLength = 25), escape = F, rownames = F
                    ))
   
-  output$tbRespostas <- renderDataTable({
-    
-    arq <- input$file1
-    
-    if(is.null(arq)){
-      return(respostas)
-    }
-    
-    return(read.csv2(arq$datapath))
-    }, options = list(
+  output$tbRespostas <- renderDataTable(respostaReac(), options = list(
     language = list(url = '//cdn.datatables.net/plug-ins/1.10.11/i18n/Portuguese.json')),
     rownames = F
   )
   
-  output$tbGabarito<- renderDataTable({
-    
-    arq <- input$file2
-    
-    if(is.null(arq)){
-      return(gabarito)
-    }
-    
-    return(read.csv2(arq$datapath))
+  output$tbGabarito<- renderDataTable({ 
+      gabaritoReac()
     }, options = list(
       language = list(url = '//cdn.datatables.net/plug-ins/1.10.11/i18n/Portuguese.json')),
     rownames = F
