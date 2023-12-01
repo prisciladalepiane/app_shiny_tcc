443fcd6ccefbcde938b1f7c74acb9e83454998e7
Priscila Dalepiane <prisciladalepiane@gmail.com>
2023-12-01 08:55:01 -0400

    Transeferencia da manipulação de dados de main para o arquivo de server

--- app/main.R
+++ app/main.R
@@ -16,35 +16,7 @@ source("funcoes.R")
 ###########################  Definir Variáveis ################################# 
 
 teste <- read.csv2("dados/respostas_teste.csv")
-gabarito <- read.csv2("dados/gabarito_teste.csv")
-
-respostas <- teste |> mutate(CodigoQuestao = str_sub(CodigoQuestao,1,3))
-gabarito <- gabarito |> mutate(Item = str_sub(Item,1,3))
-
-questoes <- gabarito$Item
-
-matriz <- respostasParaMatriz(respostas) 
-
-matriz_alternativas <- respostasParaMatriz(respostas, alternativas = T)
-
-tct_alt <- calculoTctAlternativas(matriz_alternativas, gabarito) 
-
-tct_alt <- tct_alt %>% 
-  filter(key > 0) %>% 
-  mutate(key = LETTERS[as.numeric(key)]) 
-
-tct_alternativas <- formatarTctAlternativas(tct_alt)
-
-acertos <- matriz |> select(RespondenteId) |> mutate(Acertos = rowSums(matriz[,-1]))
-
-grafico_alt <- respostas |> left_join(acertos) |>
-  group_by(Acertos,CodigoQuestao,AlternativaOrdem) |> 
-  count() |>
-  mutate(Alternativa = LETTERS[AlternativaOrdem]) 
-
-grafico_alt2 <- grafico_alt |> left_join(
-  grafico_alt |> group_by(CodigoQuestao,Acertos) |> summarise(Total = sum(n))
-) |> mutate(p = n/Total) 
+gabarito_teste <- read.csv2("dados/gabarito_teste.csv")
 
 
 ###########################  Rodar Aplicacao ################################### 
